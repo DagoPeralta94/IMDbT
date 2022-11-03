521fd5558ae3ab3a5891681604fcaa03d0a55dd3
DagoPeralta94 <dagoperalta163@gmail.com>
2022-11-03 11:10:16 -0500

    Add structure for data information

---
+++ app/src/main/java/com/example/imdbt/application/AppConstants.kt
@@ -0,0 +1,7 @@
+package com.example.imdbt.application
+
+object AppConstants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "c5c47722a4adcc77f6e84f28a48b857"
+    const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500/"
+}
