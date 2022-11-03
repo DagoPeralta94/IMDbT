19320bdee00be994270a75e0e60887faa5c6d9b4
DagoPeralta94 <dagoperalta163@gmail.com>
2022-11-03 17:54:27 -0500

    Add viewModel on LoginFragment and logical for isUserLogin

--- app/src/main/java/com/example/imdbt/presentation/MovieViewModel.kt
+++ app/src/main/java/com/example/imdbt/presentation/MovieViewModel.kt
@@ -1,24 +1,39 @@
 package com.example.imdbt.presentation
 
+import android.text.Editable
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.fragment.findNavController
+import com.example.imdbt.R
+import com.example.imdbt.data.UserProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class MovieViewModel: ViewModel() {
 
-    val txtUserVM : MutableLiveData<String> = MutableLiveData()
-    val txtPasswordVM : MutableLiveData<String> = MutableLiveData()
+    private var isLogin: Boolean = false
 
-    fun getCurrentInformation(){
-        txtUserVM
-        txtPasswordVM
+    private val _txtUserVM = MutableLiveData<String>()
+    val txtUserVM: MutableLiveData<String> get() = _txtUserVM
+
+    private val _txtPasswordVM = MutableLiveData<String>()
+    val txtPasswordVM: MutableLiveData<String> = _txtPasswordVM
+
+    fun isLoginTrue(user: String, password: String): Boolean {
+        var i = 0
+
+        for (users in UserProvider.userList) {
+
+            val userProv = UserProvider.userList[i].user
+            val passProv = UserProvider.userList[i].password
+
+            isLogin = user == userProv && passProv == password
+        }
+        return isLogin
     }
-
-    fun getTxtInformation(
-        nameLogin: String,
-        passwordLogin: String
-    ){
-        txtUserVM.value = "Hola ${nameLogin}"
-        txtPasswordVM.value = "Tu contra es ${passwordLogin}"
-    }
-
 }
--- app/src/main/java/com/example/imdbt/ui/LoginFragment.kt
+++ app/src/main/java/com/example/imdbt/ui/LoginFragment.kt
@@ -1,27 +1,27 @@
 package com.example.imdbt.ui
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
 import androidx.navigation.fragment.findNavController
 import com.example.imdbt.R
 import com.example.imdbt.data.UserProvider
 import com.example.imdbt.databinding.FragmentLoginBinding
 import com.example.imdbt.presentation.MovieViewModel
+import kotlinx.coroutines.launch
 
 
 class LoginFragment : Fragment() {
 
-    private var _binding : FragmentLoginBinding? = null
-    private val binding get()  = _binding!!
-    private lateinit var viewModel : MovieViewModel
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: MovieViewModel
+    private val user = binding.txtUser.text.toString()
+    private val password = binding.txtPassword.text.toString()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,34 +33,32 @@ class LoginFragment : Fragment() {
     }
 
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
-        viewModel.getCurrentInformation()
-        saveInformation()
+        viewModel = ViewModelProvider(this).get()
+        initObservers()
         binding.txtRegister.setOnClickListener { goingToRegister() }
         binding.login1.setOnClickListener { goingToNavigation() }
 
     }
 
+    private fun initObservers() {
+        viewModel.txtUserVM.observe(viewLifecycleOwner,
+            Observer { userOb ->
+                var texto = binding.txtUser.text.toString()
+                texto = userOb
+            })
+        viewModel.txtPasswordVM.observe(viewLifecycleOwner, Observer { passwordOb ->
+            var passObserver = binding.txtPassword.text.toString()
+            passObserver = passwordOb
+        })
+    }
+
     private fun goingToNavigation() {
-        val user0 = binding.txtUser.editText?.text.toString()
-        val passw0 = binding.txtPassword.editText?.text.toString()
-
-        var i = 0
-        for (users in UserProvider.userList) {
-
-            val user1 = UserProvider.userList[i].user
-            val passw1 = UserProvider.userList[i].password
-
-            if (user0 == user1 && passw1 == passw0) {
-                findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
-                break
-            } else {
-                Toast.makeText(binding.txtUser.context, "Usuario Incorrecto", Toast.LENGTH_SHORT).show()
-                i += 1
-            }
+        if(viewModel.isLoginTrue(user, password)){
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }else{
+            Toast.makeText(binding.login1.context, "Usuario o contraseña Incorrecta", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -68,15 +66,4 @@ class LoginFragment : Fragment() {
         findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
     }
 
-    private fun saveInformation() {
-        with(binding) {
-            viewModel.getTxtInformation(
-                "${txtUser.editText?.text}",
-                "${txtPassword.editText?.text}"
-            )
-        }
-        viewModel.txtUserVM.observe(viewLifecycleOwner, Observer {
-            binding.txtInformation.text = it
-        })
-    }
 }
