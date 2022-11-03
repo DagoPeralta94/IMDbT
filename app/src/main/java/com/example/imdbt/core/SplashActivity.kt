c6af3423f3c3c30a7f7832ae047936c1faa24626
DagoPeralta94 <dagoperalta163@gmail.com>
2022-11-03 11:30:57 -0500

    Fix launch activity

--- app/src/main/java/com/example/imdbt/core/SplashActivity.kt
+++ app/src/main/java/com/example/imdbt/core/SplashActivity.kt
@@ -1,17 +1,18 @@
 package com.example.imdbt.core
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import com.example.imdbt.databinding.ActivityMainBinding
+import com.example.imdbt.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        startActivity(Intent(this,MainActivity::class.java))
+    }
+    override fun onStop() {
+        super.onStop()
+        finish()
     }
 }
