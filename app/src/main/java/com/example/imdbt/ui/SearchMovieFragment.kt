667dcc0652ecde8077e6d7c551c31073d2c50ada
DagoPeralta94 <dagoperalta163@gmail.com>
2022-11-09 10:35:17 -0500

    Add searchView with Api and RecyclerView!

--- app/src/main/java/com/example/imdbt/ui/SearchMovieFragment.kt
+++ app/src/main/java/com/example/imdbt/ui/SearchMovieFragment.kt
@@ -7,23 +7,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.imdbt.application.AppConstants
 import com.example.imdbt.core.adapter.MovieAdapter
 import com.example.imdbt.data.model.MovieDb
+import com.example.imdbt.data.model.MovieDbClient
+import com.example.imdbt.data.model.MovieDbResult
 import com.example.imdbt.data.model.MoviesProvider
 import com.example.imdbt.databinding.FragmentSearchMovieBinding
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class SearchMovieFragment : Fragment() {
 
     private var _binding: FragmentSearchMovieBinding? = null
     private val binding get() = _binding!!
+    private var topMovies: List<MovieDbResult> = emptyList()
+    lateinit var topMoviesList: List<MovieDb>
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            lifecycleScope.launch{
-                binding.rcMovies.layoutManager = LinearLayoutManager(binding.rcMovies.context, LinearLayoutManager.VERTICAL, false)
-                binding.rcMovies.adapter = MovieAdapter(MoviesProvider.movieListBest)
+            lifecycleScope.launch {
+                var topMovies = MovieDbClient.service.listTopRatedMovies(AppConstants.API_KEY)
+                topMoviesList = topMovies.results
+                withContext(Dispatchers.Main){
+                    binding.rcMovies.layoutManager = LinearLayoutManager(binding.rcMovies.context, LinearLayoutManager.VERTICAL, false)
+                    binding.rcMovies.adapter = MovieAdapter(topMoviesList)
+                    MovieAdapter(topMoviesList).notifyDataSetChanged()
+                }
             }
         }
 
@@ -34,6 +46,10 @@ class SearchMovieFragment : Fragment() {
         ): View? {
             _binding = FragmentSearchMovieBinding.inflate(inflater, container, false)
             return binding.root
+            lifecycleScope.launch{
+
+
+            }
 
         }
     }
