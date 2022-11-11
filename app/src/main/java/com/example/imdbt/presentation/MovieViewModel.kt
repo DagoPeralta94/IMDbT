e761c17a506cf7c83136c5319a1e529d1c73813c
DagoPeralta94 <dagoperalta163@gmail.com>
2022-11-11 16:30:38 -0500

    Refactor code

--- app/src/main/java/com/example/imdbt/presentation/MovieViewModel.kt
+++ app/src/main/java/com/example/imdbt/presentation/MovieViewModel.kt
@@ -1,18 +1,14 @@
 package com.example.imdbt.presentation
 
-import android.text.Editable
-import android.widget.EditText
-import android.widget.Toast
-import androidx.lifecycle.LiveData
+import android.view.MenuItem
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.navigation.fragment.findNavController
 import com.example.imdbt.R
 import com.example.imdbt.data.UserProvider
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import com.example.imdbt.ui.HomeMovieFragment
+import com.example.imdbt.ui.PlayMovieFragment
+import com.example.imdbt.ui.ProfileMovieFragment
+import com.example.imdbt.ui.SearchMovieFragment
 
 class MovieViewModel: ViewModel() {
 
--- app/src/main/java/com/example/imdbt/ui/MovieActivity.kt
+++ app/src/main/java/com/example/imdbt/ui/MovieActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -18,25 +21,23 @@ import com.example.imdbt.core.adapter.MovieAdapter
 import com.example.imdbt.data.model.MovieDb
 import com.example.imdbt.data.model.MovieDbClient
 import com.example.imdbt.databinding.ActivityMovieBinding
+import com.example.imdbt.presentation.MovieViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.*
 
 class MovieActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMovieBinding
-    private lateinit var topMovies: List<MovieDb>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMovieBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(HomeMovieFragment())
-        updateData()
-
 
         binding.buttonNavigation.setOnItemSelectedListener { item->
             when (item.itemId) {
-                R.id.homeMovieFragment -> replaceFragment(HomeMovieFragment())
+                R.id.homeMovieFragment ->  replaceFragment(HomeMovieFragment())
                 R.id.searchMovieFragment -> replaceFragment(SearchMovieFragment())
                 R.id.playMovieFragment -> replaceFragment(PlayMovieFragment())
                 R.id.profileMovieFragment -> replaceFragment(ProfileMovieFragment())
@@ -45,6 +46,7 @@ class MovieActivity : AppCompatActivity() {
         }
 
     }
+
     private fun replaceFragment(fragment : Fragment){
 
         val fragmentManager = supportFragmentManager
@@ -52,20 +54,6 @@ class MovieActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.nav_host_fragment_container,fragment)
         fragmentTransaction.commit()
 
-
-    }
-
-    private fun updateData() {
-        lifecycleScope.launch {
-            var topMovies = MovieDbClient.service.listTopRatedMovies(AppConstants.API_KEY)
-            var i = 0
-            var movieList: MutableList<String> = mutableListOf()
-            while (i < topMovies.results.size) {
-                movieList.add(topMovies.results[i].title)
-                println(movieList)
-                i += 1
-            }
-        }
     }
 
 }
