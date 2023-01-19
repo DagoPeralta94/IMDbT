6794da5768f190f75eaf9daa243effa0439ec125
DagoPeralta94 <dagoperalta163@gmail.com>
2023-01-19 15:43:52 -0500

    Add module for register with email and password on Firebase

--- app/src/main/java/com/example/imdbt/ui/RegisterFragment.kt
+++ app/src/main/java/com/example/imdbt/ui/RegisterFragment.kt
@@ -1,32 +1,70 @@
 package com.example.imdbt.ui
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.imdbt.R
+import com.example.imdbt.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.regex.Pattern
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var auth: FirebaseAuth
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
+        binding.btRegister.setOnClickListener {
+
+            with(binding) {
+
+                val mEmail = txtEmail.text.toString()
+                val mPassword = txtPassword.text.toString()
+                val mRepeatPassword = txtPasswordVerify.text.toString()
+
+                val passwordRegex = Pattern.compile(
+                    "^" +
+                            "(?=.*[-@#$%^&+=])" +     // Al menos 1 carácter especial
+                            ".{6,}" +                // Al menos 4 caracteres
+                            "$"
+                )
+
+                if (mEmail.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()) {
+                    Toast.makeText(
+                        btRegister.context, "Ingrese un email valido.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else if (mPassword.isEmpty() || !passwordRegex.matcher(mPassword).matches()) {
+                    Toast.makeText(
+                        btRegister.context, "La contraseña es debil.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else if (mPassword != mRepeatPassword) {
+                    Toast.makeText(
+                        btRegister.context, "Confirma la contraseña.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    createAccount(mEmail, mPassword)
+                }
+            }
         }
     }
 
@@ -34,27 +72,42 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            if (currentUser.isEmailVerified) {
+                goingToHome()
+            } else {
+                goingToVerifyEmail()
+            }
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    goingToVerifyEmail()
+                } else {
+                    Toast.makeText(
+                        binding.btRegister.context, "No se pudo crear la cuenta. Vuelva a intertarlo",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
     }
+    private fun goingToHome() {
+        findNavController().navigate(R.id.action_registerFragment_to_movie_graph)
+    }
+
+    private fun goingToVerifyEmail(){
+        findNavController().navigate(R.id.action_registerFragment_to_verifyEmailFragment)
+    }
 }
+
+
